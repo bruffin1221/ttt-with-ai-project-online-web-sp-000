a7f19d5c31461e63cdffe9e55a55b013383e076a
Bruce Ruffin <bruffin1221@gmail.com>
2020-02-05 16:08:07 +0000

    Automatically backed up by Learn

---
+++ lib/players/computer.rb
@@ -0,0 +1,41 @@
+module Players
+  class Computer < Player
+
+    def move(board)
+      '1'
+      binding.pry
+    end
+
+  end
+end
+
+input=2
+cells=[" ", " ", " ",
+       " ", " ", " ",
+       " ", " ", " "]
+cells[input-1]="O"
+cells[4]="X"
+if input==1
+  cells[1]="X"
+elsif input!=8 && cells[2]==" " && cells[3]==" " && cells[5]==" " && cells[6]==" " && cells[8]==" "
+  cells[7]="X"
+elsif input==8 && cells[2]==" " && cells[3]==" " && cells[5]==" " && cells[6]==" " && cells[8]==" "
+  cells[3]="X"
+elsif input!=6
+  cells[5]="X"
+elsif input==6
+  cells[2]="X"
+elsif input==7
+  cells[9]="X"
+elsif input==9
+  cells[7]="X"
+end
+if input==2 && cells[]  cells[2]==" " && cells[3]==" " && cells[5]==" " && cells[6]==" " && cells[8]==" "
+  cells[2]="X"
+end
+
+
+
+cells=["O", "X", " ",
+       "X", "X", "O",
+       " ", "O", " "]
